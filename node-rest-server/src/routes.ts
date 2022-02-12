baa2a31e3f499e6774c2bf1e4a17d59f3d35c3e8
khomotjo <khomotjomodipa4@gmail.com>
2022-02-12 20:27:57 +0200

    user and session routes

--- node-rest-server/src/routes.ts
+++ node-rest-server/src/routes.ts
@@ -1,7 +1,21 @@
 import {Express, Request, Response} from 'express';
+import validateResource from "./middleware/validateResource";
+import {createUserHandler} from "./controllers/user.controller";
+import {createUserSchema} from "./schemas/user.schema";
+import {createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler} from "./controllers/session.crotroller";
+import {createSessionSchema} from "./schemas/session.schema";
+import requireUser from "./middleware/requireUser";
 
 function routes(app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+    app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+    app.post("/api/sessions", validateResource(createSessionSchema), createUserSessionHandler);
+
+    app.get("/api/sessions", requireUser, getUserSessionsHandler);
+
+    app.delete("/api/sessions", requireUser, deleteSessionHandler);
 }
 
 export default routes
