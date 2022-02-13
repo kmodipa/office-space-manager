0e98be9a58f59d8c4b2ae969c9fa95d7fc7cfa59
khomotjo <khomotjomodipa4@gmail.com>
2022-02-13 18:39:19 +0200

    default & all-office route

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/main/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'all-offices', component: HomeComponent},
+  {path: '', redirectTo: 'all-offices', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
