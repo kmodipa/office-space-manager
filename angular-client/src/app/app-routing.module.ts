a1502c0b4cbc8ea5e0df03831796f6e0ad7b06b5
khomotjo <khomotjomodipa4@gmail.com>
2022-02-13 23:58:34 +0200

    added login-singup routes

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {HomeComponent} from "./components/main/home/home.component";
 import {AddOfficeComponent} from "./components/main/add-office/add-office.component";
 import {EditOfficeComponent} from "./components/main/edit-office/edit-office.component";
 import {OfficeComponent} from "./components/main/office/office.component";
+import {LoginComponent} from "./components/auth/login/login.component";
+import {SignupComponent} from "./components/auth/signup/signup.component";
 
 const routes: Routes = [
   {path: 'all-offices', component: HomeComponent},
   {path: 'office/:id', component: OfficeComponent},
   {path: 'add-office', component: AddOfficeComponent},
   {path: 'edit-office/:id', component: EditOfficeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signUp', component: SignupComponent},
   {path: '', redirectTo: 'all-offices', pathMatch: 'full'}
 ];
 
