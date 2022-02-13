0d58b3cdc7361582f63fd9a66852d69f5e5f278e
khomotjo <khomotjomodipa4@gmail.com>
2022-02-13 18:43:09 +0200

    app-module update -> components-module, services-module

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/main/home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ComponentsModule} from "./components/components.module";
+import {ServicesModule} from "./services/services.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    ServicesModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
