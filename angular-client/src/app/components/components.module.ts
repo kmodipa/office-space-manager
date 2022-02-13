f7c8545e4d897f4b4a378cf5734cd9a30b292636
khomotjo <khomotjomodipa4@gmail.com>
2022-02-13 19:34:19 +0200

    office-worker-service

--- angular-client/src/app/components/components.module.ts
+++ angular-client/src/app/components/components.module.ts
@@ -2,9 +2,11 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from "./main/home/home.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {OfficeAccordionComponent} from "./shared/office-accordion/office-accordion.component";
 
 export const components = [
-  HomeComponent
+  HomeComponent,
+  OfficeAccordionComponent
 ]
 
 @NgModule({
--- angular-client/src/app/services/services.module.ts
+++ angular-client/src/app/services/services.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {OfficeHttpService} from "./http/office-service/office.service";
+import {OfficeWorkerHttpService} from "./http/office-worker-service/office-worker.service";
 
 export const servicesImports = [
-  OfficeHttpService
+  OfficeHttpService,
+  OfficeWorkerHttpService
 ];
 
 @NgModule({
