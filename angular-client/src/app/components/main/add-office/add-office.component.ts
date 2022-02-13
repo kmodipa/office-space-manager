a838fe4bd0d2fc8b6d82a94cdce501c76456a400
khomotjo <khomotjomodipa4@gmail.com>
2022-02-13 23:58:16 +0200

    added login on navbar

--- angular-client/src/app/components/main/add-office/add-office.component.ts
+++ angular-client/src/app/components/main/add-office/add-office.component.ts
@@ -73,7 +73,7 @@ export class AddOfficeComponent implements OnInit {
     this.newOfficeForm = this.formBuilder.group({
       officeName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
       physicalAddress: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
-      emailAddress: ['',         [Validators.required,
+      emailAddress: ['', [Validators.required,
         Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')]],
       phoneNumber: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(12)]],
       maximumCapacity: ['', [Validators.required, Validators.min(1)]],
--- angular-client/src/app/components/shared/navbar/navbar.component.ts
+++ angular-client/src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -10,13 +11,31 @@ export class NavbarComponent implements OnInit {
 
   @Input() label!: string;
 
-  constructor(private location: Location) { }
+  constructor(private location: Location,
+              private router: Router,
+              public activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    this.router.navigate(['login']);
+  }
+
+  signUp(): void {
+    this.router.navigate(['signUp']);
+  }
+
   back(): void {
     this.location.back();
+  }
+
+  loginCondition(): boolean {
+    if (this.activatedRoute.snapshot.url[0].path != 'login' &&
+      this.activatedRoute.snapshot.url[0].path != 'signUp') {
+      return true;
+    }
+    return false;
   }
 
 }
